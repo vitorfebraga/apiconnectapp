4aff056bf3fb9f6f7d5410e98757c55b6e6aabdc
syber3048 <vitorferreira.braga@hotmail.com>
2020-04-27 20:13:02 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,77 @@
+import React,{useEffect,useState} from 'react';
+import{SafeAreaView,FlatList,Text,StyleSheet,StatusBar,TouchableOpacity} from 'react-native';
+
+import api from './services/api';
+
+export default function App(){
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    });
+  }, []);
+
+  async function handleAddProject(){
+    //percorre o array projects e copia 
+    //setProjects([...projects,`Novo projeto ${Date.now()}`]);
+    const response = await api.post('projects',{
+      title: `Novo projeto ${Date.now()}`,
+      owner: "Vitor Braga Pinto"
+    });
+    const project=response.data;
+  
+    setProjects([...projects,project])
+  
+    
+  }
+  return (<>
+
+  <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
+
+  <SafeAreaView style={styles.container}>
+    <FlatList
+    data={projects}
+    keyExtractor={project=>project.id}
+    renderItem={({item: project})=>(
+      <Text style={styles.project}>{project.title}</Text>
+    )}
+    />
+    <TouchableOpacity activeOpacity={0.6}
+    style={styles.button}
+    onPress={handleAddProject}>
+
+   <Text style={styles.buttonText}>Adicionar projeto</Text>
+    </TouchableOpacity>
+
+    </SafeAreaView>
+    </>
+  );
+}
+ 
+
+const styles=StyleSheet.create({
+container:{
+  flex:1,
+  backgroundColor:'#7159c1',
+  
+},
+
+project:{
+color:'#0f0f0f',
+fontSize:60,
+fontWeight:'bold'
+},
+button:{
+  backgroundColor:'#FFF',
+  margin:20,
+  height:50,
+  borderRadius:4,
+  justifyContent:'center',
+  alignItems:'center'
+},
+buttonText:{
+  fontWeight:'bold',
+  fontSize:18,
+}
+});
